a3004dec0b1b73a7e38b3a67aee28d51fe79d7aa
Neil-Chen <v009008777@hotmail.com>
2022-04-30 18:35:44 +0800

    finish output

--- 101.Homework/C++_OOP/4/Time.cpp
+++ 101.Homework/C++_OOP/4/Time.cpp
@@ -1,4 +1,5 @@
 #include <stdexcept>
+#include <iomanip>
 
 #include "Time.h"
 
@@ -22,14 +23,44 @@ const Time Time::operator++(int dummy)
 
 Time &Time::operator--()
 {
-    --second;
+    try
+    {
+        if (second > 0)
+        {
+            --second;
+        }
+        else
+        {
+            throw std::invalid_argument("Error: Current Time can not be negative");
+        }
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
     return *this;
 }
 
 const Time Time::operator--(int dummy)
 {
     Time old(*this);
-    --second;
+    try
+    {
+        if (second > 0)
+        {
+            --second;
+        }
+        else
+        {
+            throw std::invalid_argument("Error: Current Time can not be negative");
+        }
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
     return old;
 }
 
@@ -67,16 +98,80 @@ int operator-(const Time &t1, const Time &t2)
 
 std::ostream &operator<<(std::ostream &os, const Time &t)
 {
-    os << t.second;
+    // unix timestamp
+    // GMT + 0
+    int seconds = t.second;
+    int daysInMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int second = t.second % 60;
+    seconds -= second;
+
+    int minite = t.second / 60 % 60;
+    seconds -= minite * 60;
+
+    int hour = t.second / 3600 % 12;
+    seconds -= hour * 3600;
+
+    int month = 1, day = 1;
+    int year = 1970;
+
+    auto isLeapYear = [](int year)
+    {
+        return (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0));
+    }; // lambda expreesion
+    int secondsInYear = 31536000;
+    int secondsInLeapYear = 31622400;
+    while (seconds >= secondsInYear)
+    {
+        if (isLeapYear(year))
+        {
+            seconds -= secondsInLeapYear;
+        }
+        else
+        {
+            seconds -= secondsInYear;
+        }
+        ++year;
+    }
+
+    if (isLeapYear(year))
+    {
+        daysInMonth[1] = 29; // Feb
+    }
+    int secondsInDay = 86400;
+    for (int i = 0; seconds > daysInMonth[i] * secondsInDay; i++)
+    {
+        month++;
+        seconds -= daysInMonth[i] * secondsInDay;
+    }
+    day += seconds / 86400;
+
+    // ISO 8601
+    os << year << '-' << std::setw(2) << std::setfill('0') << month << '-' << std::setw(2) << day << ' ' << std::setw(2) << hour << ':' << std::setw(2) << minite << ':' << std::setw(2) << second;
     return os;
 }
 
 bool operator>(const Time &t1, const Time &t2)
 {
+    return (t1.second > t2.second);
 }
 
-bool operator>=(const Time &t1, const Time &t2);
-bool operator==(const Time &t1, const Time &t2);
-bool operator!=(const Time &t1, const Time &t2);
-bool operator<(const Time &t1, const Time &t2);
-bool operator<=(const Time &t1, const Time &t2);
+bool operator>=(const Time &t1, const Time &t2)
+{
+    return (t1.second >= t2.second);
+}
+bool operator==(const Time &t1, const Time &t2)
+{
+    return (t1.second == t2.second);
+}
+bool operator!=(const Time &t1, const Time &t2)
+{
+    return (t1.second != t2.second);
+}
+bool operator<(const Time &t1, const Time &t2)
+{
+    return (t1.second < t2.second);
+}
+bool operator<=(const Time &t1, const Time &t2)
+{
+    return (t1.second <= t2.second);
+}
--- 101.Homework/C++_OOP/4/test.cpp
+++ 101.Homework/C++_OOP/4/test.cpp
@@ -4,29 +4,54 @@ using namespace std;
 
 int main()
 {
-    int x = -1;
-    int b = 1;
+    int seconds = 1651226715;
+    int daysInMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int second = seconds % 60;
+    seconds -= second;
 
-    // Some code
-    cout << "Before try \n";
-    try
+    int minite = seconds / 60 % 60;
+    seconds -= minite * 60;
+
+    int hour = seconds / 3600 % 12;
+    seconds -= hour * 3600;
+
+    int month = 1, day = 1;
+    int year = 1970;
+
+    auto isLeapYear = [](int year)
     {
-        cout << "Inside try \n";
-        if (b > x)
+        return (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0));
+    }; // lambda expreesion
+    int secondsInYear = 31536000;
+    int secondsInLeapYear = 31622400;
+    while (seconds >= secondsInYear)
+    {
+        if (isLeapYear(year))
         {
-            b -= x;
+            seconds -= secondsInLeapYear;
         }
         else
         {
-            throw x;
-            cout << "After throw (Never executed) \n";
+            seconds -= secondsInYear;
         }
-    }
-    catch (int x)
-    {
-        cout << "Exception Caught \n";
+        ++year;
     }
 
-    cout << b;
-    return 0;
+    if (isLeapYear(year))
+    {
+        daysInMonth[1] = 29; // Feb
+    }
+    int secondsInDay = 86400;
+    for (int i = 0; seconds > daysInMonth[i] * secondsInDay; i++)
+    {
+        month++;
+        seconds -= daysInMonth[i] * secondsInDay;
+    }
+    day += seconds / 86400;
+    cout << year << endl;
+    cout << month << endl;
+    cout << day << endl;
+    cout << hour << endl;
+    cout << minite << endl;
+    cout << second << endl;
 }
