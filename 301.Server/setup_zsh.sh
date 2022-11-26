1c468d688b7c98aa54353a818216f462771f2c66
Neil-Chen <v009008777@hotmail.com>
2022-11-26 20:22:56 +0800

    feat: add p10k

--- 301.Server/setup_zsh.sh
+++ 301.Server/setup_zsh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Usage: 
-# wget https://github.com/qazxcdswe123/DeathRoadToBaldness/raw/main/301.Server/setup_zsh.sh -O ~/.config/setup_zsh.sh
+# wget https://github.com/qazxcdswe123/DeathRoadToBaldness/raw/main/301.Server/setup_zsh.sh -O ~/.config/setup_zsh.sh && chmod +x ~/.config/setup_zsh.sh && ~/.config/setup_zsh.sh
 
 # check os type
 if [[ $(uname) == "Linux" ]]; then
@@ -15,11 +15,13 @@ fi
 
 # install zsh
 if [[ $os_type == "linux" ]]; then
-    sudo apt install zsh
+    apt update && apt install zsh -y
 fi
 
 wget https://github.com/qazxcdswe123/DeathRoadToBaldness/raw/main/301.Server/.zshrc -O ~/.zshrc
 wget https://github.com/qazxcdswe123/DeathRoadToBaldness/raw/main/301.Server/.zimrc -O ~/.zimrc
+wget https://github.com/qazxcdswe123/DeathRoadToBaldness/raw/main/301.Server/.p10k.zsh -O ~/.p10k.zsh
 
 # change default shell
 chsh -s $(which zsh)
+zsh
